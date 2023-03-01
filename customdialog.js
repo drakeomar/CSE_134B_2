777ca0fdea1744cf511d759745900a9f25556c26
drakeomar <38961142+drakeomar@users.noreply.github.com>
2023-02-28 20:34:56 -0800

    creating all new files for hw4, and nativedialogs.html buttons/html

--- customdialog.js
+++ customdialog.js
@@ -1,3 +1,23 @@
 export function customdialog() {
     alert(`Hello, ${user}!`);
+    let testStr =
+        `<dialog id="favDialog">
+        <form method="dialog">
+        <p>
+        <label>Favorite animal:
+        <select>
+          <option value="default">Choose…</option>
+          <option>Brine shrimp</option>
+          <option>Red panda</option>
+          <option>Spider monkey</option>
+        </select>
+      </label>
+    </p>
+    <div>
+      <button value="cancel">Cancel</button>
+      <button id="confirmBtn" value="default">Confirm</button>
+    </div>
+  </form>
+</dialog>`;
+    document.getElementById('main-display').innerHTML(testStr);
 }
