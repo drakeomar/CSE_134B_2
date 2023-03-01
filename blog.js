85b12388e950e0e652a0572a521c7bd54f84f8c7
drakeomar <38961142+drakeomar@users.noreply.github.com>
2023-03-01 00:26:15 -0800

    creating all new files for hw4, and nativedialogs.html buttons/html

--- blog.js
+++ blog.js
@@ -86,7 +86,7 @@ function createBlogPost(){
 // "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
     confirmBtn.addEventListener('click', () => {
         if(titleInput.value !== "" && dateInput.value !== "" && summaryInput.value !== "" ){
-            let newPost = `<div style="display:inline"><h1>${titleInput.value}:</h1>
+            let newPost = `<div style="display:inline; border: solid black 1px"><h1>${titleInput.value}:</h1>
                        <p>${dateInput.value}</p>
                        <p>${summaryInput.value}</p>
                         </div>`;
