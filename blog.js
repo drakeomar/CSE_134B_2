15625a76f4a14323ee6edfc4141d38fcc4ef05c4
drakeomar <38961142+drakeomar@users.noreply.github.com>
2023-02-28 22:30:53 -0800

    creating all new files for hw4, and nativedialogs.html buttons/html

--- blog.js
+++ blog.js
@@ -1,8 +1,13 @@
-class test{
 
-}
 function blogMain(){
-
+    Storage.prototype.setObj = function(key, obj) {
+        return this.setItem(key, JSON.stringify(obj))
+    }
+    Storage.prototype.getObj = function(key) {
+        return JSON.parse(this.getItem(key))
+    }
+    localStorage.setObj(`test`, `1`)
+    console.log(localStorage.getObj(`test`))
 }
 function createBlogPost(){
 
@@ -13,4 +18,4 @@ function deleteBlogPost(){
 function updateBlogPost(){
 
 }
-export {test}
+export {blogMain, createBlogPost, deleteBlogPost, updateBlogPost}
