499b5f7ee5a23d99790c97a166515161a098b1e3
drake <daomar@ucsd.edu>
2023-03-01 23:28:48 -0800

    updating crud

--- hw4/README.md
+++ hw4/README.md
@@ -1,20 +1,40 @@
+
+    NAME: DRAKE OMAR 
+
+    PID: A15326724
+
+    NETLIFY LINK: 
+
 Part 1:
+
+    Prompt vs Safer Prompt: 
+        The main difference between the two is the lack of DOM purification or 
+        "sanitization" in the regular prompt displayed. This means that potentially
+        malicious code entered as user input may be able to be injected, and, in
+        general, the regular prompt is more susceptible to XSS attacks. The Safer
+        Prompt takes advantage of DOMPurify in order to remove potentially harmful 
+        bits of HTML all in order to scrub user input. 
+
+    For the tagged template literals, 
 
 Part 2:
 
+
+    
 Part 3:
 
+        For prepopulating the localStorage array and page with dummy posts, I simply initialized the
+        array within localStorage storing all the posts with 4 generic blog postings.
+        If this requirement did not exist, and/or if persistence across page loads was a requirement,
+        then I would have simply populated the page with whatever exists in the array "posts"
+        within localStorage.
 Part 4: 
 
 Part 5: 
 
-For the tagged template literals, 
 
-For prepopulating the localStorage array and page with dummy posts, I simply initialized the
-array within localStorage storing all the posts with 4 generic blog postings. 
-If this requirement did not exist, and/or if persistence across page loads was a requirement, 
-then I would have simply populated the page with whatever exists in the array "posts" 
-within localStorage.
 
-I included the third party JS library Anime found at 
+
+
+    I included the third party JS library Anime found at 
 
--- hw4/customdialog.js
+++ hw4/customdialog.js
@@ -1,39 +1,100 @@
 
 function customdialogMain() {
+    const outputBox = document.getElementById('cs-output');
+    const alertBtn = document.getElementById('custom-alert-button');
+    const confirmBtn = document.getElementById('custom-confirm-button');
+    const promptBtn = document.getElementById('custom-prompt-button');
+
     let testStr =
-        `<dialog style="display: block" id="favDialog">
-        <form method="dialog">
-        <p>
-        <label>Favorite animal:
-        <select>
-          <option value="default">Choose…</option>
-          <option>Brine shrimp</option>
-          <option>Red panda</option>
-          <option>Spider monkey</option>
-        </select>
-      </label>
-    </p>
-    <div>
-      <button value="cancel">Cancel</button>
-      <button id="confirmBtn" value="default">Confirm</button>
-    </div>
-  </form>
-</dialog>`;
+        `<dialog style="display: block" id="csDialog">
+            <form method="dialog">
+                <p>
+                    <label>What is your name?</label>
+                    <input type="text">
+                </p>
+                <div>
+                    <button value="cancel">Cancel</button>
+                    <button id="cs-confirm" value="default">Confirm</button>
+                </div>
+            </form>
+        </dialog>`;
 
-    document.getElementById('main-display').setHTML(testStr,new Sanitizer());
-}
+    /**listners*/
+    alertBtn.addEventListener('click', () => {
+        alertDialog(outputBox);
+    });
+    confirmBtn.addEventListener('click', () => {
+        confirmDialog(outputBox);
+    });
+    promptBtn.addEventListener('click', () => {
+        promptDialog(outputBox);
+    });
 
-function alertDialog(){
-    return 0;
-}
-function confirmDialog(){
-    return 0;
-}
-function confirmConfirm(){
-    document.querySelector('dialog').style.display = 'none';
-}
-function promptDialog(){
-    return 0;
+    //document.getElementById('main-display').setHTML(testStr,new Sanitizer());
 }
 
+function alertDialog(outputBox){
+    let testStr =
+        `<dialog id="custom-alert-dialog" style="display: block" id="csAlertDialog">
+            <form method="dialog">
+                <h2> ALERT! </h2>
+                <p>
+                   You are getting an URGENT MESSAGE!!
+                </p>
+                <div>
+                    <button id="cs-cancel" value="cancel">Cancel</button>
+                    <button id="cs-confirm" value="default">Confirm</button>
+                </div>
+            </form>
+        </dialog>`;
+
+    bindButtons(outputBox);
+    return 0;
+}
+function confirmDialog(outputBox){
+    let testStr =
+        `<dialog id="custom-confirm-dialog" style="display: block" id="csConfirmDialog">
+            <form method="dialog">
+                <h2>Do you confirm to the terms and conditions?</h2>
+                <div>
+                    <button value="cancel">Cancel</button>
+                    <button id="cs-confirm" value="default">Confirm</button>
+                </div>
+            </form>
+        </dialog>`;
+
+    document.getElementById('cs-output').innerHTML = testStr;
+    document.getElementById('custom-confirm-dialog').show();
+    return 0;
+}
+function promptDialog(outputBox){
+    let testStr =
+        `<dialog style="display: block" id="csPromptDialog">
+            <form method="dialog">
+                <p>
+                    <label>What is your name?</label>
+                    <input type="text">
+                </p>
+                <div>
+                    <button value="cancel">Cancel</button>
+                    <button id="cs-confirm" value="default">Confirm</button>
+                </div>
+            </form>
+        </dialog>`;
+
+    return 0;
+}
+
+function bindButtons(){
+    const confirmBtn = document.getElementById('cs-confirm');
+    const cancelBtn = document.getElementById('cs-cancel');
+
+    confirmBtn.addEventListener('click', () => {
+
+    });
+    cancelBtn.addEventListener('click', () => {
+
+    });
+
+}
 export {customdialogMain, confirmDialog, alertDialog, promptDialog}
